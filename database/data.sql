c3872e6ce709ea025916884f0d87deb9cef853cb
Alex Ehrenkranz <alex.ehrenkranz+git@gmail.com>
2024-04-16 03:10:55 +0000

    initial commit

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,16 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+ insert into "classes"
+ ("name","vigor","mind","endurance","strength","dexterity","intelligence","faith","arcane")
+ values
+ ('Vagabond',15,10,11,14,13,9,9,7),
+ ('Warrior',11,12,11,10,16,10,8,9),
+ ('Hero',14,9,12,16,9,7,8,11),
+ ('Bandit',10,11,10,9,13,9,8,14),
+ ('Astrologer',9,15,9,8,12,16,7,9),
+ ('Prophet',10,14,8,11,10,7,16,10),
+ ('Samurai',12,11,13,12,15,9,8,8),
+ ('Prisoner',11,12,11,11,14,14,6,9),
+ ('Confessor',10,13,10,12,12,9,14,9),
+ ('Wretch',10,10,10,10,10,10,10,10);
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,42 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+CREATE TABLE "classes" (
+  "name" text,
+  "vigor" integer,
+  "mind" integer,
+  "endurance" integer,
+  "strength" integer,
+  "dexterity" integer,
+  "intelligence" integer,
+  "faith" integer,
+  "arcane" integer,
+  "id" serial PRIMARY KEY
+);
+
+CREATE TABLE "builds" (
+  "userId" serial PRIMARY KEY,
+  "classId" serial,
+  "buildName" text,
+  "characterName" text,
+  "vigor" integer,
+  "mind" integer,
+  "endurance" integer,
+  "strength" integer,
+  "dexterity" integer,
+  "intelligence" integer,
+  "faith" integer,
+  "arcane" integer,
+  "created_at" timestamp
+);
+
+CREATE TABLE "users" (
+  "id" serial PRIMARY KEY,
+  "username" text,
+  "hashedPassword" text,
+  "created_at" timestamp
+);
+
+ALTER TABLE "builds" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+
+ALTER TABLE "builds" ADD FOREIGN KEY ("classId") REFERENCES "classes" ("id");
